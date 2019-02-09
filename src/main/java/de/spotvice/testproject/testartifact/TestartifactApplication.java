d0c49884dea5b1ede945b172400b3576442e6161
Thomas Engelhard <thomas.engelhard@adesso.de>
2019-02-09 16:04:48 +0100

    initial commit

---
+++ src/main/java/de/spotvice/testproject/testartifact/TestartifactApplication.java
@@ -0,0 +1,12 @@
+package de.spotvice.testproject.testartifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestartifactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestartifactApplication.class, args);
+    }
+}
