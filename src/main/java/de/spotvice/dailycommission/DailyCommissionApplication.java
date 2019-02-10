2a81806974388acbb7bf43034572060250d784a3
Thomas Engelhard <thomas.engelhard@adesso.de>
2019-02-10 23:15:59 +0100

    added h2 database

--- src/main/java/de/spotvice/dailycommission/DailyCommissionApplication.java
+++ src/main/java/de/spotvice/dailycommission/DailyCommissionApplication.java
@@ -1,52 +1,23 @@
 package de.spotvice.dailycommission;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import javax.ws.rs.core.Application;
 
 @SpringBootApplication
 public class DailyCommissionApplication {
-    private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(DailyCommissionApplication.class, args);
     }
 
+    /* Adding dummy data to database
     @Bean
-    public CommandLineRunner demo(ColleagueRepository repository) {
-        return (args) -> {
-            // save a couple of customers
+    public boolean createTestData(ColleagueRepository repository) {
             repository.save(new Colleague("Arsentos", "Miglyn"));
             repository.save(new Colleague("Tomatos", "Hard"));
             repository.save(new Colleague("Dobias", "Pom"));
             repository.save(new Colleague("Boerdi", "Voght"));
             repository.save(new Colleague("Dueclos", "Prinz aus Kamerun"));
-
-            // fetch all customers
-            log.info("Colleague found with findAll():");
-            log.info("-------------------------------");
-
-            for (Colleague colleague : repository.findAll()) {
-                log.info(colleague.toString());
-            }
-
-            log.info("");
-
-            // fetch an individual customer by ID
-            repository.findByFirstName("Dobias")
-                    .forEach(colleague -> {
-                        log.info("Colleague found with findByFirstName(\"Dobias\"):");
-                        log.info("--------------------------------");
-                        log.info(colleague.toString());
-                        log.info("");
-                    });
-
-            log.info("");
-        };
-    }
+            return true;
+    }*/
 }
---
+++ src/test/java/de/spotvice/dailycommission/DatabaseTests.java
@@ -0,0 +1,40 @@
+package de.spotvice.dailycommission;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.inject.Inject;
+import javax.ws.rs.core.Application;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Thomas Engelhard
+ * <p>
+ * For testing with actual database: @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+ */
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@Transactional
+public class DatabaseTests {
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    @Inject
+    ColleagueRepository repository;
+
+    @Test
+    public void saveToRepository() {
+        repository.save(new Colleague("Arsentos", "Miglyn"));
+        repository.save(new Colleague("Tomatos", "Hard"));
+        repository.save(new Colleague("Dobias", "Pom"));
+        repository.save(new Colleague("Boerdi", "Voght"));
+        repository.save(new Colleague("Dueclos", "Prinz aus Kamerun"));
+
+        assertEquals(5, repository.findAll().size());
+    }
+}
