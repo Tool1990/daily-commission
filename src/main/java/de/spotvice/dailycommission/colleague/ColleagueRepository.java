a3db36f52cdc85e3ac93fb04cc6b199c388334f9
Thomas Engelhard <thomas.engelhard@adesso.de>
2019-02-21 18:40:29 +0100

    minor changes

--- src/main/java/de/spotvice/dailycommission/colleague/ColleagueRepository.java
+++ src/main/java/de/spotvice/dailycommission/colleague/ColleagueRepository.java
@@ -1,5 +1,6 @@
 package de.spotvice.dailycommission.colleague;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,5 +9,6 @@ import java.util.UUID;
 public interface ColleagueRepository extends JpaRepository<Colleague, UUID> {
     List<Colleague> findByFirstName(String firstName);
 
+    @Cacheable()
     List<Colleague> findByLastName(String lastName);
 }
--- src/test/java/de/spotvice/dailycommission/DatabaseTests.java
+++ src/test/java/de/spotvice/dailycommission/DatabaseTests.java
@@ -8,7 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.inject.Inject;
 import javax.ws.rs.core.Application;
@@ -20,7 +19,6 @@ import static org.junit.Assert.assertEquals;
  */
 @RunWith(SpringRunner.class)
 @DataJpaTest
-@Transactional
 public class DatabaseTests {
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
